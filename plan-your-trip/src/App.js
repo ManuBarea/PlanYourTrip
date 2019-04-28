52dd1d467e671a98c77c46b573e08efcd7e0de0f
Manuel Outeiriño Barneto <manueloute@MBP-de-Manuel.home>
2019-04-28 23:24:53 +0200

    se resume el cambio de estado al tratar la respuesta de la busqueda

--- plan-your-trip/src/App.js
+++ plan-your-trip/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
         longitude: -4.074039,
         zoom: 6
       },
-      points: [],
       venues:[],
       searching: false
     }
@@ -27,10 +26,10 @@ class App extends Component {
   handleSearch = (searchData) => {
     this.setState({ searching: true });
 
-   
+
     let query = searchData.query;
     let categories = searchData.categories;
-    
+
     VenuesClient.search( query,categories)
       .then((data) => {
         if (!data.meta || data.meta.code !== 200) {
@@ -38,37 +37,34 @@ class App extends Component {
           this.setState({ searching: false });
         } else {
           console.log('received venues', data);
-          this.setState({ points: data.response.venues.map(entry=>({
-            latitude: entry.location.lat,
-            longitude: entry.location.lng})),
-          searching: false });
-          this.setState({ venues: data.response.venues, searching: false });
-          
-          let newViewport = {...this.state.viewport};
-          newViewport.latitude = data.response.geocode.feature.geometry.center.lat;
-          newViewport.longitude = data.response.geocode.feature.geometry.center.lng;
-          newViewport.zoom= 15;
-         
-          console.log('latitude',newViewport);
-          this.setState({ viewport: newViewport});
-
-          
+          this.setState({
+            venues: data.response.venues.map(entry=> ({
+              latitude: entry.location.lat,
+              longitude: entry.location.lng
+            })),
+            searching: false,
+            viewport: {
+              latitude: data.response.geocode.feature.geometry.center.lat,
+              longitude: data.response.geocode.feature.geometry.center.lng,
+              zoom: 12
+            }
+          });
         }
       }, (error) => {
         console.log('error searching points', error);
         this.setState({ searching: false });
-      }); 
+      });
   }
 
   render() {
-    const { viewport, points, searching } = this.state;
+    const { viewport, venues, searching } = this.state;
 
     console.log(viewport);
 
     return (
       <div className="App">
         <SearchBar locked={ searching } onSearch={ this.handleSearch } />
-        <Map viewport={ viewport } points={ points } onViewportChange={ (viewport) => !searching && this.setState({ viewport }) } />
+        <Map viewport={ viewport } points={ venues } onViewportChange={ (viewport) => !searching && this.setState({ viewport }) } />
       </div>
     );
   }
