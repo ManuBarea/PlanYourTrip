e24e011a55702a4094fe1af4e67de6210722b619
Manuel Barea López <39103838+ManuBarea@users.noreply.github.com>
2019-05-25 22:32:26 +0200

    Update venues.controller.js

--- plan-your-trip/src/controllers/venues.controller.js
+++ plan-your-trip/src/controllers/venues.controller.js
@@ -2,6 +2,7 @@ import VenuesClient from '../client/venues.client';
 
 import Category from '../model/category.model';
 import Venue from '../model/venue.model';
+import VenuePhoto from '../model/venuePhoto.model';
 
 export default class VenuesController {
 
@@ -32,6 +33,20 @@ export default class VenuesController {
             }
           }, (error) => reject(error));
       });
+    }
+    static searchPhoto(query) {
+      return new Promise((resolve, reject) => {
+        VenuesClient.searchPhoto(query)
+          .then((data) => {
+            if (!data.meta || data.meta.code !== 200) {
+              reject(new Error('invalid response status'));
+            } else {
+              console.log('received venue photo', data);
+              resolve(data.response.photos.items.map(photo => new VenuePhoto(photo)));
+            }
+          })
+          .catch((error) => reject(error));
+      })
     }
 
 }
