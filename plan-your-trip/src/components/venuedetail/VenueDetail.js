6e2cc018f7c2cc75dc2396e778ad4b811774147e
Manuel Barea López <39103838+ManuBarea@users.noreply.github.com>
2019-05-25 22:32:14 +0200

    Update VenueDetail.js

--- plan-your-trip/src/components/venuedetail/VenueDetail.js
+++ plan-your-trip/src/components/venuedetail/VenueDetail.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
 import Dispatcher from '../../mixins/dispatcher';
-
+import VenuesController from '../../controllers/venues.controller';
 import './VenueDetail.scss';
+import SearchPhotos from './SearchPhotos';
 
 export default class VenueDetail extends Component {
 
@@ -26,6 +27,17 @@ export default class VenueDetail extends Component {
         { venue.categories.length && venue.categories.map((category, i) => (<div key={i}>
           <img src={ category.icon.buildUrl(64) } title={ category.name } />
         </div>)) }
+      </div>  
+      <div className="venuePhoto">
+        { VenuesController.searchPhoto(venue.id)}
+      </div>
+
+      <div className="photos">
+        <button > Related photographies
+        {<SearchPhotos venue={venue.name}></SearchPhotos>}
+        </button>
+        
+        
       </div>
     </div>);
   }
